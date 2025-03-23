363b2f872f8d481a4b72930d1bff00bc5ab7dbd6
Thebeatkicks <000henrik@gmail.com>
2025-03-23 15:51:57 +0100

    removed the assignment

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-/* Create a simple program that returns a price tag from the user, then give them a discount of 10 procent 
-
- 
-
-    Task: 
-
-    1. Ask the user to enter a price tag using prompt(), for example: $49.99 
-
-    2. Convert the input from string to number 
-
-    3. Reduce the price by 10 procent 
-
-    4. Print out the new price using console.log(), for example: 'Your new price is: $44.99' */
-
 let input = prompt("How much?", "$");
 
 console.log(input);
